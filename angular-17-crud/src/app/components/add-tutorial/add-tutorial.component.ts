3040b5dc901621a9ae15a496d479b5fc25f0879b
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2024-07-03 15:31:57 +0700

    build angular 17 service and component

---
+++ angular-17-crud/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tutorial',
+  templateUrl: './add-tutorial.component.html',
+  styleUrl: './add-tutorial.component.css'
+})
+export class AddTutorialComponent {
+
+}
---
+++ angular-17-crud/src/app/components/tutorial-details/tutorial-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-details',
+  templateUrl: './tutorial-details.component.html',
+  styleUrl: './tutorial-details.component.css'
+})
+export class TutorialDetailsComponent {
+
+}
