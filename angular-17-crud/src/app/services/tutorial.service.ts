b841903454bce0a8ebeeba72641e986878fac04d
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2024-07-09 11:04:42 +0700

    comfig dockerfile

--- angular-17-crud/src/app/services/tutorial.service.ts
+++ angular-17-crud/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/toturials';
+const baseUrl = 'http://localhost:6868/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
--- mongodb/src/main/java/com/topzson/spring/data/mongodb/controller/TutorialController.java
+++ mongodb/src/main/java/com/topzson/spring/data/mongodb/controller/TutorialController.java
@@ -7,12 +7,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.topzson.spring.data.mongodb.model.Tutorial;
 import com.topzson.spring.data.mongodb.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8081" , allowedHeaders = {"Authorization", "Origin"},maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 
